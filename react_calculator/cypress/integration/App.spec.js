6c28670524908ebf5a43341167e72fa7b769e4f9
Joshua <jssmiller94@gmail.com>
2021-05-12 21:27:46 +0100

    almost finished ui tests, now need to display -error- when dividing by zero

--- react_calculator/cypress/integration/App.spec.js
+++ react_calculator/cypress/integration/App.spec.js
@@ -7,4 +7,39 @@ describe("Calculator", () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
   })
+
+  it('should do arithmetical operations', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_add').click();
+    cy.get('#number2').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '4')
+  })
+
+  it('should chain operations together', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_add').click();
+    cy.get('#number2').click();
+    cy.get('#operator_equals').click();
+    cy.get('#operator_multiply').click();
+    cy.get('#number4').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '16')
+  })
+
+  it('should handle all kinds of numbers', () => {
+    cy.get('#number1').click();
+    cy.get('#operator_divide').click();
+    cy.get('#number2').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '0.5')
+  })
+
+  it('should return error if you divide by zero', () => {
+    cy.get('#number1').click();
+    cy.get('#operator_divide').click();
+    cy.get('#number0').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', 'Error')
+  })
 })
